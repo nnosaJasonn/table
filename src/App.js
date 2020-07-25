10e420c74da4ee529b4b80258798c786be69f913
lindsey.jason.andrew@gmail.com <lindsey.jason.andrew@gmail.com>
2020-07-24 23:15:27 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -4,199 +4,101 @@ import axios from 'axios';
 import './App.css'
 const columns = [
     {
-        label: 'Action',
-        name: 'action',
-        type: 'action',
-        sortable: false
-    },
-    {
         label: 'Id',
         name: 'Id',
-        type: 'number',
-        sortable: true,
-    },
-    {
-        label: 'AutoStatus',
-        name: 'AutoStatus',
-        type: 'enum',
-        sortable: false,
-    },
-    {
-        label: 'AutoCreatedTime',
-        name: 'AutoCreatedTime',
-        type: 'date',
-        sortable: true,
-    },
-    {
-        label: 'AutoUpdatedTime',
-        name: 'AutoUpdatedTime',
-        type: 'date',
-        sortable: true,
-    },
-    {
-        label: 'AlertShortDesc',
-        name: 'AlertShortDesc',
-        type: 'text',
-        sortable: false,
-    },
-    {
-        label: 'AlertLongDesc',
-        name: 'AlertLongDesc',
-        type: 'text',
-        sortable: false,
-    },
-    {
-        label: 'AlertHost',
-        name: 'AlertHost',
-        type: 'text',
-        sortable: true,
-    },
-    {
-        label: 'AlertTriggeredTime',
-        name: 'AlertTriggeredTime',
-        type: 'date',
-        sortable: true,
-    },
-    {
-        label: 'AlertID',
-        name: 'AlertID',
         type: 'id',
         sortable: false,
     },
     {
-        label: 'AlertSource',
-        name: 'AlertSource',
-        type: 'text',
+        label: 'External Id',
+        name: 'External_Id',
+        type: 'id',
         sortable: false,
     },
     {
-        label: 'AlertCompany',
-        name: 'AlertCompany',
-        type: 'text',
-        sortable: false,
-    },
-    {
-        label: 'SDTicketNum',
-        name: 'SDTicketNum',
-        type: 'text',
-        sortable: false,
-    },
-    {
-        label: 'SDID',
-        name: 'SDID',
+        label: 'Name',
+        name: 'Name',
         type: 'text',
         sortable: true,
     },
     {
-        label: 'SDStatus',
-        name: 'SDStatus',
-        type: 'enum',
-        sortable: false,
+        label: 'Date Created',
+        name: 'Date_Created',
+        type: 'date',
+        sortable: true
     },
     {
-        label: 'SDSupportGroup',
-        name: 'SDSupportGroup',
-        type: 'enum',
-        sortable: false,
-    },
-    {
-        label: 'AlertType',
-        name: 'AlertType',
-        type: 'enum',
-        sortable: false,
-    },
-    {
-        label: 'AlertSubtype',
-        name: 'AlertSubtype',
-        type: 'enum',
-        sortable: false,
-    },
-    {
-        label: 'AlertSeverity',
-        name: 'AlertSeverity',
-        type: 'enum',
-        sortable: false,
-    },
-    {
-        label: 'AutoAction',
-        name: 'AutoAction',
-        type: 'enum',
-        sortable: false,
-    },
-    {
-        label: 'AutoNote',
-        name: 'AutoNote',
-        type: 'enum',
-        sortable: false,
-    },
-    {
-        label: 'JATicketNum',
-        name: 'JATicketNum',
-        type: 'text',
-        sortable: false,
-    },
-    {
-        label: 'JAID',
-        name: 'JAID',
-        type: 'text',
-        sortable: false,
-    },
-    {
-        label: 'JAStatus',
-        name: 'JAStatus',
-        type: 'enum',
-        sortable: false
-    },
-    {
-        label: 'LinkStatus',
-        name: 'LinkStatus',
-        type: 'text',
-        sortable: false,
-    },
-    {
-        label: 'CommentStatus',
-        name: 'CommentStatus',
-        type: 'text',
-        sortable: false,
-    },
-    {
-        label: 'AlertSubCompany',
-        name: 'AlertSubCompany',
-        type: 'text',
-        sortable: false,
-    },
-    {
-        label: 'Location',
-        name: 'Location',
-        type: 'text',
-        sortable: false,
-    },
-    {
-        label: 'State',
-        name: 'State',
-        type: 'text',
-        sortable: false,
+        label: 'Status',
+        name: 'Status',
+        type: 'boolean',
+        sortable: true
     }
 ];
 
+const rows = [
+    {
+        Id: '123',
+        External_Id: 'abc',
+        Name: 'Bob',
+        Date_Created: '07/21/2020',
+        Status: 'Open'
+    },
+    {
+        Id: '234',
+        External_Id: 'def',
+        Name: 'Janis',
+        Date_Created: '07/21/2020',
+        Status: 'Closed'
+    },
+    {
+        Id: '345',
+        External_Id: 'fgh',
+        Name: 'Elijah',
+        Date_Created: '07/20/2020',
+        Status: 'Open'
+    },
+    {
+        Id: '678',
+        External_Id: 'hij',
+        Name: 'zeke',
+        Date_Created: '07/10/2020',
+        Status: 'Closed'
+    },
+    {
+        Id: '098',
+        External_Id: 'zyx',
+        Name: 'Karen',
+        Date_Created: '06/21/2020',
+        Status: 'Closed'
+    },
+    {
+        External_Id: 'wvu',
+        Date_Created: '07/13/2019',
+        Id: '987',
+        Status: 'Closed',
+        Name: 'Moses'
+    },
+    {
+        Id: '876',
+        External_Id: 'kjl',
+        Name: 'Byrd',
+        Date_Created: '06/30/2020',
+        Status: 'Open'
+    },
+    {
+        Id: '765',
+        External_Id: 'lji',
+        Name: 'Buggy Bug',
+        Date_Created: '07/07/2020',
+        Status: 'Closed'
+    }
+]
+
 
 export default () => {
-    const [rows, setRows] = useState(null);
-    const sortBy = 'Id';
-    const sortType = 'number';
+    const sortBy = 'Date_Created';
+    const sortType = 'date';
 
-    useEffect(() => {
-        const  func = async () => {
-
-            const res = await axios('http://localhost:8888/getRows');
-            res.data.forEach(row => {
-                row['action'] = <button onClick={() => alert(row['Id'])} type="button" class="btn btn-light scrollable"><i class="material-icons">send</i></button>
-
-            })
-            setRows(res.data);
-        }
-        func();
-    }, [])
 
     if(rows) {
         return (
